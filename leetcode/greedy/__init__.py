85247c5c3f403f73c2808f44f1d34c02007ecb7a
estelle18 <jimingshu218@163.com>
2022-02-22 18:44:20 +0800

    init

---
+++ leetcode/greedy/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3.6
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2022/2/22 18:41
+@Author  : estelle.ji
+@Email   : mingshu.ji@amh-group.com
+@File    : __init__.py.py
+@Software: PyCharm
+"""
